826b66ab2587e665aed53c7ddab245c80c0d10c3
sanjay-sba3 <sanjay.sba3@gmail.com>
2022-01-30 14:31:12 +0530

    commting dimond

---
+++ dimond.py
@@ -0,0 +1,17 @@
+x = int(input('Enter numbers of rows : '))
+k = x+1
+for i in range(0, x+1):
+    for j in range(0, k):
+        print(end=' ')
+    k = k-1
+    for j in range(0, i+1):
+        print('*', end=' ')
+    print(' ')
+k = k+2
+for i in range(x, 0 ,-1):
+    for j in range(0, k):
+        print(end=' ')
+    k = k+1
+    for j in range (i, 0, -1):
+        print('*', end=' ')
+    print(' ')
