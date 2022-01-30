d2c8be56077444747ae3108f742b009f5a647105
sanjay-sba3 <sanjay.sba3@gmail.com>
2022-01-30 14:49:12 +0530

    commiting by dev2

---
+++ star_and_reverce.py
@@ -0,0 +1,9 @@
+x = int(input('Enter numbers of row :'))
+for i in range(0, x):
+    for j in range(0, i+1):
+        print('*', end=' ')
+    print(' ')
+for i in range(x, 0, -1):
+    for j in range(1, i):
+        print('*', end=' ')
+    print(' ')
