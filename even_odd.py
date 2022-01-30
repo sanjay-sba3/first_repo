a30f6eb54b23199d619b91f29343aa7f4bc5bfd0
sanjay-sba3 <sanjay.sba3@gmail.com>
2022-01-30 14:34:38 +0530

    commiting even_odd

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+num = int(input('enter the number that wants to test for even or odd:'))
+test = num % 2
+if test == 0:
+    print('entered number is even')
+else:
+    print('entered number is odd')
