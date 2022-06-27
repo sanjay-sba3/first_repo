e7f8969a0cce65770de2d5effd472f85ae48592b
sanjay-sba3 <sanjay.sba3@gmail.com>
2022-06-27 14:01:07 +0530

    thsi is new trail commit by sanjay

--- Cal_n!.py
+++ Cal_n!.py
@@ -2,5 +2,5 @@ x = int(input('enter the number for which factorial is to be calculate :'))
 c = 1
 for i in range(1, x+1):
     c = c * i
-print('Factorial of', '"', x, '"', ' is', '"', c, '"')
+print(f'Factorial of {x} is'{c}')
 print("Thank you very much")
